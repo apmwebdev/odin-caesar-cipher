e1ea7ed456b36eb4f959d91ee389801e4e2f3cc4
Austin Miller <austinmillerwebdev@gmail.com>
2022-11-09 08:18:40 -0800

    Create caesar cypher using input string and shift from user

--- cipher.rb
+++ cipher.rb
@@ -1,1 +1,30 @@
+def caesar_cipher(string, shift)
+  string_ascii = string.bytes
+  encoded_str = string_ascii.map { |char| encode_char(char, shift) }
+  puts encoded_str.pack('c*')
+end
+def encode_char(char, shift)
+  case
+  when char.between?(97, 122)
+    shifted_char = char + shift
+    if shifted_char > 122
+      shifted_char = 96 + shifted_char - 122
+    end
+    return shifted_char
+  when char.between?(65, 90)
+    shifted_char = char + shift
+    if shifted_char > 90
+      shifted_char = 64 + shifted_char - 90
+    end
+    return shifted_char
+  else
+    return char
+  end
+end
 
+puts "Enter string:"
+input_string = gets.chomp
+puts "Enter shift:"
+input_shift = gets.chomp.to_i
+
+caesar_cipher(input_string, input_shift)
